ac3ddd842de1f9b5136925edf41cf00035d191db
ivankhomichkov <ivan.khomichkov@gmail.com>
2026-04-09 23:57:52 +0400

    ADD: add location hotspot prefabs and expand locations - create hotspot prefabs for 7 new locations - add front_porch, living_room, backyard, barn, paddock, tractor_field, jasons_house, garage locations - add location transitions and hotspot definitions - add background video assets for new locations - reorganize hotspot prefabs into dedicated folder - update LocationsConfig with new location entries - remove obsolete BackgroundRef and OnEnterScript fields

--- Assets/_Project/Scripts/Locations/Infrustructure/DataAccess/LocationDefinition.cs
+++ Assets/_Project/Scripts/Locations/Infrustructure/DataAccess/LocationDefinition.cs
@@ -8,11 +8,9 @@ namespace OnlyFarms.Locations
     [Serializable]
     public class LocationDefinition
     {
-        public string Id; //TODO: String identifier not good
-        [Obsolete][FormerlySerializedAs("VideoRef")] public AssetReference BackgroundRef;
+        public string Id;
         public string BackgroundName;
         public AssetReference HotspotPrefabRef;
-        public string OnEnterScript; //TODO: String identifier not good
         public bool HasBackButton;
         public HotspotEntry[] Hotspots;
         public float TransitionDuration = 1f;
